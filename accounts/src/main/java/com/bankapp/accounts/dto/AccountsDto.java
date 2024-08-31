de1cf98689328ccbcb88943aa93c61c35e3b931a
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-08-30 20:02:38 -0400

    Added validations

--- accounts/src/main/java/com/bankapp/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/bankapp/accounts/dto/AccountsDto.java
@@ -1,10 +1,19 @@
 package com.bankapp.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+    @NotEmpty(message = "Account Number can not be null or empty.")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Account number must be 10 digits.")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type can not be null or empty.")
     private String accountType;
+
+    @NotEmpty(message = "Account branch address can not be null or empty.")
     private String branchAddress;
+
 }
