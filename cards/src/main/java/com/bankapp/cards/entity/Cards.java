784009033b8f8545941c83d55886524b1c0be081
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-09-03 10:26:08 -0400

    Created the cards entity and sql schema

---
+++ cards/src/main/java/com/bankapp/cards/entity/Cards.java
@@ -0,0 +1,25 @@
+package com.bankapp.cards.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Cards {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+    private String mobileNumber;
+    private String cardNumber;
+    private String cardType;
+    private Long totalLimit;
+    private Long amountUsed;
+    private Long availableAmount;
+}
