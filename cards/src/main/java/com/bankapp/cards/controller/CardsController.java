43b43797dbf08224e6ac06fd38a325a5a3d3f782
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-09-04 16:27:00 -0400

    added api documentation with open api

--- cards/src/main/java/com/bankapp/cards/controller/CardsController.java
+++ cards/src/main/java/com/bankapp/cards/controller/CardsController.java
@@ -2,8 +2,15 @@ package com.bankapp.cards.controller;
 
 import com.bankapp.cards.constants.CardsConstants;
 import com.bankapp.cards.dto.CardsDto;
+import com.bankapp.cards.dto.ErrorResponseDto;
 import com.bankapp.cards.dto.ResponseDto;
 import com.bankapp.cards.service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,7 +20,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(
+        name = "CRUD REST API fro Cards in BankApp",
+        description = "CRUD REST APIs in BankApp to CREATE, UPDATE, FETCH and DELETE Cards details."
+)
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
@@ -21,6 +31,14 @@ import java.util.List;
 public class CardsController {
     private ICardsService iCardsService;
 
+    @Operation(
+            summary = "Create Card REST API.",
+            description = "REST API to create new Card inside BankApp."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED."
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createCard(@RequestParam
                                                       @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits.")
@@ -31,6 +49,14 @@ public class CardsController {
                 .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Card REST API.",
+            description = "REST API to fetch a Card details based on a given card number."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK."
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
                                                         @Pattern(regexp = "(^$|[0-9]{12})",message = "Card number must be 12 digits.")
@@ -39,6 +65,14 @@ public class CardsController {
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
+    @Operation(
+            summary = "Fetch List of Card REST API.",
+            description = "REST API to fetch a List of Card details based on a given mobile number."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK."
+    )
     @GetMapping("/fetchAll")
     public ResponseEntity<List<CardsDto>> fetchAllCardDetails(@RequestParam
                                                             @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits.")
@@ -46,6 +80,28 @@ public class CardsController {
         List<CardsDto> cardsDto = iCardsService.fetchAllCardsFromMobileNumber(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
+
+    @Operation(
+            summary = "Update Card details REST API.",
+            description = "REST API to update a Card details based on a given card object."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK."
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP Status Expectation Failed."
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error.",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateCardDetails(@Valid @RequestBody CardsDto cardsDto){
         boolean isUpdated = iCardsService.updateCard(cardsDto);
@@ -55,10 +111,32 @@ public class CardsController {
                     .body(new ResponseDto(CardsConstants.STATUS_200,CardsConstants.MESSAGE_200));
         }else {
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(CardsConstants.STATUS_500,CardsConstants.MESSAGE_500));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardsConstants.STATUS_417,CardsConstants.MESSAGE_417_UPDATE));
         }
     }
+
+    @Operation(
+            summary = "Delete Card details REST API.",
+            description = "REST API to Delete a Card details based on a given mobile number."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK."
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP Status Expectation Failed."
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error.",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteCardDetails(@RequestParam
                                                             @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits.")
@@ -70,8 +148,8 @@ public class CardsController {
                     .body(new ResponseDto(CardsConstants.STATUS_200,CardsConstants.MESSAGE_200));
         }else {
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(CardsConstants.STATUS_500,CardsConstants.MESSAGE_500));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardsConstants.STATUS_417,CardsConstants.MESSAGE_417_DELETE));
         }
     }
 }
--- cards/src/main/java/com/bankapp/cards/dto/CardsDto.java
+++ cards/src/main/java/com/bankapp/cards/dto/CardsDto.java
@@ -1,31 +1,59 @@
 package com.bankapp.cards.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
-
+@Schema(
+        name = "Cards",
+        description = "Schema to hold Card information"
+)
 @Data
 public class CardsDto {
 
+    @Schema(
+            description = "Mobile user of the customer",
+            example = "1234567890"
+    )
     @NotEmpty(message = "Card mobile number cannot be null or empty.")
     @Pattern(regexp = "(^$|[0-9]{10})",message = "Card mobile number must be 10 digits.")
     private String mobileNumber;
 
+    @Schema(
+            description = "Card number of the customer.",
+            example = "123456789012"
+    )
     @NotEmpty(message = "Card number cannot be null or empty.")
     @Pattern(regexp = "(^$|[0-9]{12})",message = "Card number must be 12 digits.")
     private String cardNumber;
 
+    @Schema(
+            description = "Type of the card.",
+            example = "Credit Card"
+    )
     @NotEmpty(message = "Card type cannot be null or empty.")
     private String cardType;
 
+    @Schema(
+            description = "Total amount limit available against a card.",
+            example = "100000"
+    )
     @Positive(message = "Total limit should be greater than zero.")
     private Long totalLimit;
 
+    @Schema(
+            description = "Total amount used by a customer",
+            example = "1000"
+    )
     @PositiveOrZero(message = "Total amount used should be equal or greater than zero.")
     private Long amountUsed;
 
+    @Schema(
+            description = "Total available amount against a card.",
+            example = "90000"
+    )
     @PositiveOrZero(message = "Total available amount should be equal or greater than zero.")
     private Long availableAmount;
 }
