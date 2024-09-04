9a0fea72719595d2e8e0f3095841468d280db424
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-09-04 16:35:18 -0400

    upgraded documentation

--- cards/src/main/java/com/bankapp/cards/CardsApplication.java
+++ cards/src/main/java/com/bankapp/cards/CardsApplication.java
@@ -1,9 +1,31 @@
 package com.bankapp.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "BankApp Cards microservice REST API Documentation.",
+                version = "v1",
+                contact = @Contact(
+                        name = "Ruddy Gomez",
+                        email = "rgomeztejada@outlook.com",
+                        url = "swagger.io"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "BankApp Cards microservice REST API Documentation.",
+                url = "swagger.io"
+        )
+)
 public class CardsApplication {
 
     public static void main(String[] args) {
--- cards/src/main/java/com/bankapp/cards/controller/CardsController.java
+++ cards/src/main/java/com/bankapp/cards/controller/CardsController.java
@@ -15,6 +15,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +26,7 @@ import java.util.List;
         description = "CRUD REST APIs in BankApp to CREATE, UPDATE, FETCH and DELETE Cards details."
 )
 @RestController
-@RequestMapping("/api")
+@RequestMapping(path = "/api",produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
 public class CardsController {
