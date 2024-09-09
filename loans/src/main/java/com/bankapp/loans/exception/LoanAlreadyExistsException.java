b953fc6552e29871c3e5d9017ce24f2cbdac9e9e
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-09-09 17:50:13 -0400

    Added Exception handling

---
+++ loans/src/main/java/com/bankapp/loans/exception/LoanAlreadyExistsException.java
@@ -0,0 +1,16 @@
+package com.bankapp.loans.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class LoanAlreadyExistsException extends RuntimeException {
+    /**
+     * Constructs a new runtime exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
+    public LoanAlreadyExistsException(String message) {
+        super(message);
+    }
+}
